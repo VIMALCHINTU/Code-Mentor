bed93090f6f265e1864a1c30d98c17ba3579d99d
VIMALCHINTU <vimalchintu24@gmail.com>
2026-08-29 16:13:49 +0530

    Prepare backend for deployment

--- popup/src/App.jsx
+++ popup/src/App.jsx
@@ -64,7 +64,7 @@ setPageData(null);
   };
 
   const response = await fetch(
-    "http://localhost:5000/analyze",
+    "https://code-mentor-c20a.onrender.com/analyze",
     {
       method: "POST",
 
